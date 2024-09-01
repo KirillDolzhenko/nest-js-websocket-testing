e07d456b57712f2249e32b70a4232e99c751282d
KirillDolzhenko <manfeed3@gmail.com>
2024-09-01 15:00:09 +0300

    Added broadcasting

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,13 +1,30 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { Socket } from 'socket.io';
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { ClientRequest, Server } from 'http';
 
 @WebSocketGateway(3010, {
-  cors: false
+  cors: {
+    origin: "*"
+  }
 })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect{
+  @WebSocketServer() server: Server;
+
   @SubscribeMessage("newMessage")
-  handleNewMessage(@MessageBody() message: any) {
-    console.log((message))
+  handleNewMessage(client: Socket, message: any) {
+    console.log(client)
+    console.log(message)
+
+    this.server.emit("message", message)
   }
 
-  
+  handleConnection(client: any, ...args: any[]) {
+    this.server.emit("user-join", `User ${client.id} joined chatroom`)
+  }
+
+  handleDisconnect(client: any) {
+    this.server.emit("user-left", `User ${client.id} left chatroom`)
+  }
+
+
 }
